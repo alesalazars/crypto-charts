0c366d1d0238371ae4aed6db8097811836b5627e
Ale <ma.salazarsanchez@gmail.com>
2020-04-29 01:29:55 -0400

    Made adjustments to functions logic

--- src/pages/functions.js
+++ src/pages/functions.js
@@ -3,51 +3,68 @@ import React from 'react'
 import AssetItem from '../components/asset-item'
 
 
-const assetSearch = (event, query, setQuery, setAssets) => {
+// Get data promise
+const getData = (q) => {
+  return(
+    fetch(`https://economicapp.io/api/asset/search/?${q}=bit`,
+      {
+        method: 'GET',
+        headers:{
+          'Authorization': 'Token 26704736d3a5c8712dc149fb67643608f0397267'
+        }
+      }
+    )
+    .then(response => response.json())
+  )
+}
 
-  let lastValue = event.target.value
-  console.log('lastValue: ', lastValue)
-  setQuery(lastValue)
 
-  console.log('query: ', query)
+let lastValue = null
+
+// Define last inputted full value
+const defineInput = (val, query, setQuery) => {
+
+  lastValue = val
+  console.log('val: ', val)
 
   setTimeout(() => {
-    if(query === lastValue){
-      console.log(`Consideraremos: ${lastValue}`)
-
-      getData(setAssets)
-
+    if(lastValue === val){
+      console.log(`Consideraremos: ${val}`)
+      setQuery(val)
+      console.log('query es: ', query)
     }
   }, 3000)
 
 }
 
 
-const getData = async(setAssets) => {
-  const response = await fetch('https://economicapp.io/api/asset/assets/?limit=50&offset=0',
-    {
-      method: 'GET',
-      headers:{
-        'Authorization': 'Token 26704736d3a5c8712dc149fb67643608f0397267'
-      }
-    }
-  )
-  const data = await response.json()
-  console.log('data ==> ', data)
-  setAssets(data)
-}
-
-
-const renderSearchResults = (assets, query) => {
-  assets.forEach(item => {
-    if(item.name === query){
-      // return
-      //   {AssetItem key={item.key}/}
-      console.log('wa')
-    }
+// Return promise result from fetch
+const getAssets = (query) => {
+  const promise = getData(query)
+  promise.then(data => {
+    console.log('data ==> ', data)
+    // setAssets(data)
   })
 }
 
 
+// Search assets
+const assetSearch = (event, query, setQuery) => {
+  let val = event.target.value
+  defineInput(val, query, setQuery)
+  getAssets(query)
+}
 
-export { assetSearch, renderSearchResults };
+
+// const renderSearchResults = (assets, query) => {
+//   assets.forEach(item => {
+//     if(item.name === query){
+//       // return
+//       //   {AssetItem key={item.key}/}
+//       console.log('wa')
+//     }
+//   })
+// }
+
+
+export { assetSearch };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import Searchbar from '../components/searchbar';
-import { assetSearch, renderSearchResults } from './functions';
+import { assetSearch } from './functions';
 
 import './index.css';
 
@@ -13,17 +13,20 @@ const Index = () => {
   // Text input in searchbar
   const [query, setQuery] = useState('')
 
-  // Pagination
-  // const [pageNumber, setPageNumber] = useState(1)
-
 
   return(
     <div className="index">
       <div className="sidebar">
         <h1 className="color-white bg-main logo f-uppercase color-white bg-main"><span className="f-bold">Crypto</span><span className="f-light">Charts</span></h1>
-        <Searchbar onChange={ (event) => {assetSearch(event, query, setQuery, setAssets, assets)} } value={query}/>
+        <Searchbar 
+          onChange={ (event) => {
+            assetSearch(event, query, setQuery)
+            // assetSearch(event, query, setAssets)
+          } } 
+          // value={query}
+        />
         <ul>
-          {renderSearchResults(assets, query)}
+          {/* {renderSearchResults(assets, query)} */}
         </ul>
       </div>
       <div className="content">
